2299b687b9e80ddfb9495b7cf53c92183e02d7c1
skxsephiroth <skxsephiroth87@gmail.com>
2018-02-26 14:27:03 +0800

    --Q20b, Q22, Q29, indent subquery --improve Q24 discountAmount = unitprice*discount --add Q28 selfjoin with subquery --corrrected Q34 --Q35 remove indentation on subquery

--- SQLAdvancedQueryOnNorthwindWorkshopQ17-36.sql
+++ SQLAdvancedQueryOnNorthwindWorkshopQ17-36.sql
@@ -42,9 +42,9 @@ order by productid, productname
 select *
 from products
 where CategoryID in
-(select CategoryID 
-from Categories 
-where categoryname = 'beverages' or categoryname = 'condiments') 
+	(select CategoryID 
+	 from Categories 
+	 where categoryname = 'beverages' or categoryname = 'condiments') 
 order by productid, productname
 
 select *
@@ -66,9 +66,9 @@ where country = 'usa'
 select *
 from orders  
 where shipvia = 2 and EmployeeID in
-(select EmployeeID  
-from Employees  
-where title = 'sales representative') 
+	(select EmployeeID  
+	 from Employees  
+	 where title = 'sales representative') 
 
 --Q23
 select staff.lastname, manager.lastname as manager
@@ -79,6 +79,11 @@ on staff.reportsto = manager.EmployeeID
 select top 5 *
 from [Order Details]  
 order by discount desc
+
+select productid, unitprice*discount as discountInDollars
+from [Order Details]
+group by productid, discountInDollar  
+order by discountInDollars desc
 
 --Q25
 select customers.companyname, suppliers.city
@@ -115,18 +120,30 @@ where orderid = 10248
 
 select *
 from Employees 
-where EmployeeID  = 5
+where EmployeeID = 5
 
 select lastname
 from Employees 
 where employeeid = 2
 
+select manager.lastname
+from Employees staff join Employees manager
+on staff.ReportsTo = manager.EmployeeID
+where staff.reportsto in
+	(select ReportsTo
+	 from Employees 
+	 where employeeid in
+		(select employeeid
+		 from orders 
+		 where orderid = 10248)
+	)
+
 --Q29
 select *
 from products
 where unitprice > 
-(select avg(unitprice)
-from products)
+	(select avg(unitprice)
+	 from products)
 
 --Q30
 select *, unitprice * quantity as amount
@@ -154,12 +171,20 @@ on [Order Details].orderid = orders.OrderID
 group by CustomerID 
 
 --Q34
+/*
 select avg(amount)
 from (
 	select orderid, sum(unitprice * quantity) as amount
 	from [Order Details]  
 	group by orderid
-	) as inner_query
+	) as inner_query */ --this gives the average of each order id, not grouped by customer id
+select avg(amount)
+from (
+	 select customerid, sum(unitprice * quantity) as amount
+	 from [Order Details] join Orders 
+	 on [Order Details].orderid = orders.OrderID
+	 group by CustomerID 
+	 ) as inner_query
 
 --Q35
 select sum(unitprice * quantity) as amount, orders.customerid, companyname
@@ -168,15 +193,7 @@ on [Order Details].orderid = orders.OrderID
 join customers
 on orders.customerid = customers.customerid
 group by orders.customerid, companyname
---having sum(unitprice * quantity) > 1632.0946
-having sum(unitprice * quantity) > (
-									select avg(amount)
-									from (
-										select orderid, sum(unitprice * quantity) as amount
-										from [Order Details]  
-										group by orderid
-										) as inner_query
-									)
+having sum(unitprice * quantity) > 15220.6583
 
 --Q36
 select customerid, sum(unitprice * quantity) as amount
@@ -184,4 +201,3 @@ from [Order Details] join Orders
 on [Order Details].orderid = orders.OrderID
 where year(orders.orderdate) = 1997
 group by CustomerID 
-
